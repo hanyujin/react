3a56b1112cd38068dd3eedeaf5b24c7d8861babf
YujinHan <godss39822@naver.com>
2018-10-08 17:08:54 +0900

    ch05.  componentDidMount를 이용한 json 데이터세팅

---
+++ ch05/users/js/script.js
@@ -0,0 +1,6 @@
+let dataUrl = '/react/ch05/users/real-user-data.json';
+ReactDOM.render(React.createElement(
+  'div',
+  null,
+  React.createElement(Users, { 'data-url': dataUrl })
+), document.getElementById('content'));
